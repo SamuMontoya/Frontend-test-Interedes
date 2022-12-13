01103d0e9c2b943a25114ce506c98ada26bcf642
SamuMontoya <samu7montoya@gmail.com>
2022-12-13 14:28:40 -0500

    thirdCommit

---
+++ components/modal-component.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-modal id="modal" :title="subject.name">
+    <b-row class="mx-2 mt-2">
+      <label>Username</label>
+      <b-form-input type="text" disabled :placeholder="subject.username" />
+    </b-row>
+    <b-row class="mx-2 mt-2">
+      <label>Email</label>
+      <b-form-input type="text" disabled :placeholder="subject.email" />
+    </b-row>
+    <b-row class="mx-2 mt-2">
+      <label>Address</label>
+      <b-form-input type="text" disabled :placeholder="subject.address" />
+    </b-row>
+    <b-row class="mx-2 mt-2">
+      <label>Phone</label>
+      <b-form-input type="text" disabled :placeholder="subject.phone" />
+    </b-row>
+    <b-row class="mx-2 mt-2">
+      <label>Website</label>
+      <b-form-input type="text" disabled :placeholder="subject.website" />
+    </b-row>
+    <b-row class="mx-2 mt-2">
+      <label>Company</label>
+      <b-form-input type="text" disabled :placeholder="subject.company" />
+    </b-row>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  props: ["subject"],
+};
+</script>
--- components/users-component.vue
+++ components/users-component.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">
+    <modal-component :subject="subject" />
+
     <h1 class="mt-5 text-center">Users</h1>
     <b
       ><b-row class="mt-5 bg-primary text-light">
@@ -15,17 +17,32 @@
       <b-col>{{ r.name }}</b-col>
       <b-col>{{ r.address.street }}</b-col>
       <b-col>{{ r.email }}</b-col>
-      <b-col class="text-center"><a href="/">ℹ️</a></b-col>
+      <b-col 
+      class="text-center" 
+      @click="showModal(); modalData(r)"
+      >ℹ️</b-col>
     </b-row>
   </div>
 </template>
 
 <script>
+import modalComponent from './modal-component.vue';
 export default {
+  components: { modalComponent },
   name: "users-component",
   data() {
     return {
       response: null,
+      title: 'Hello Modal',
+      subject: {
+        name: null,
+        username: null,
+        email: null,
+        address: null,
+        phone: null,
+        website: null,
+        company: null,
+      },
     };
   },
   methods: {
@@ -35,6 +52,18 @@ export default {
       const response = await this.$axios.get(api);
       this.response = response.data;
     },
+    showModal() {
+      this.$root.$emit("bv::show::modal", "modal");
+    },
+    modalData(r){
+      this.subject.name = r.name
+      this.subject.username = r.username
+      this.subject.email = r.email
+      this.subject.address = r.address.street
+      this.subject.phone = r.phone
+      this.subject.website = r.website
+      this.subject.company = r.company.name
+    }
   },
   async mounted() {
     this.asyncData();
